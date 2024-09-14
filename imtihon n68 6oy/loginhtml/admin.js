07635fcbdd1300a8af91524ce852489a444e65b6
G'ulomov Asrorbek <168567537+asrorcik2007@users.noreply.github.com>
2024-09-14 19:22:28 +0500

    Add files via upload

---
+++ imtihon n68 6oy/loginhtml/admin.js
@@ -0,0 +1,30 @@
+let products = [];
+
+document.getElementById("adminForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const productName = document.getElementById("productName").value;
+  const productPrice = document.getElementById("productPrice").value;
+  const productDescription =
+    document.getElementById("productDescription").value;
+
+  const product = {
+    name: productName,
+    price: productPrice,
+    description: productDescription,
+  };
+
+  products.push(product);
+  renderProducts();
+});
+
+function renderProducts() {
+  const productList = document.getElementById("productList");
+  productList.innerHTML = ""; // Ro'yxatni tozalash
+
+  products.forEach((product) => {
+    const li = document.createElement("li");
+    li.textContent = `${product.name} - $${product.price}: ${product.description}`;
+    productList.appendChild(li);
+  });
+}
---
+++ imtihon n68 6oy/loginhtml/login.js
@@ -0,0 +1,25 @@
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const response = await fetch("https://fakeapi.platzi.com/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+
+    const data = await response.json();
+
+    if (data.token) {
+      localStorage.setItem("token", data.token);
+      window.location.href = "admin.html"; // Admin sahifasiga o'tish
+    } else {
+      alert("Login xato");
+    }
+  });
